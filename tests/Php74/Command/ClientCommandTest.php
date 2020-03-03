d6801813eda2aa9c47775e13dc7c1491102ecd5f
Matteo <m.galacci@gmail.com>
2020-03-03 23:09:54 +0100

    Setup phpunit

---
+++ tests/Php74/Command/ClientCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Command;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientCommandTest extends TestCase
+{
+   /**
+    * @test
+    */
+   public function foo()
+   {
+      $a = 1;
+
+      $this->assertTrue(true);
+   }
+}
